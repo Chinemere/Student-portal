d2459f4f1059b054f127c02e9a2bd47279b7f38c
Chinemere Chukwu <chukwuchinemere@hmail.com>
2023-03-19 01:51:17 +0100

    heroku commit

--- runserver.py
+++ runserver.py
@@ -3,5 +3,5 @@ from api.config import config_dict
 
 app = create_app(config_dict["prod"])
 
-if __name__== '__main__':
-    app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run()
